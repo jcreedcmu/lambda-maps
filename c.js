31b9c78ca7257d80b709931cef19107fcfe656d2
Jason Reed <jcreed@gmail.com>
2016-02-12 10:49:07 -0500

    everything in one

--- c.js
+++ c.js
@@ -5,7 +5,7 @@ function fact(a) {
 function choose(a, b) {
   return fact(a) / fact(b) / fact(a - b);
 }
-function memoize(c) {
+function memoize3(c) {
   var cache = {};
   function cc(n, k, e) {
 	 var ky = n+","+k+","+e;
@@ -16,10 +16,22 @@ function memoize(c) {
   }
   return cc;
 }
-
-var cc_atom = memoize(c_atom);
-var cc_normal = memoize(c_normal);
-var cc_struct = memoize(c_struct);
+function memoize2(c) {
+  var cache = {};
+  function cc(n, k) {
+	 var ky = n+","+k;
+	 if (cache[ky] == null) {
+		cache[ky] = c(n, k);
+	 }
+	 return cache[ky];
+  }
+  return cc;
+}
+var cc_norm = memoize2(c_norm);
+var cc_atom = memoize2(c_atom);
+var cc_vert = memoize3(c_vert);
+var cc_edge = memoize3(c_edge);
+var cc_struct = memoize3(c_struct);
 
 function prod(l1, l2) {
   rv = [];
@@ -31,23 +43,54 @@ function prod(l1, l2) {
   return rv;
 }
 
+
 // atomic terms
-// n = # lambdas, k = # free atomic vars, e = # free normal vars
-function c_atom(n, k, e) {
+// n = # lambdas, k = # free vars
+function c_atom(n, k) {
+  if (n < 0) return [];
+
+  if (n == 0 && k == 1) {
+	 return [["var"]];
+  }
+  var rv = [];
+  for (var a = 0; a <= n; a++) {
+	 for (var b = 0; b <= k; b++) {
+
+		if (a == 0 && b == 0) continue;
+		if (a == n && b == k) continue;
+		var ex = prod(cc_atom(a, b), cc_norm(n - a, k - b));
+		rv = rv.concat( ex );
+	 }
+  }
+  return rv;
+}
+
+// normal terms
+// n = # lambdas, k = # free vars
+function c_norm(n, k) {
+  if (n < 0) return [];
+
+  return cc_atom(n,k).concat(cc_norm(n-1, k+1 , true).map(function(x) { return ["lam", x] }));
+
+}
+
+// vertic terms
+// n = # lambdas, k = # free vertic vars, e = # free edge vars
+function c_vert(n, k, e) {
   if (n < 0) return [];
   if (k == 0) e = false;
 
   if (n == 0 && k == 1 && !e) {
 	 return [["var"]];
   }
-  var rv = cc_normal(n - 1, k + 1, e).map(function(x) { return ["lam", x] });
+  var rv = cc_edge(n - 1, k + 1, e).map(function(x) { return ["lam", x] });
 
   for (var a = 0; a <= n; a++) {
 	 for (var b = 0; b <= k; b++) {
 
 		if (a == 0 && b == 0) continue;
 		if (a == n && b == k) continue;
-		var ex = prod(cc_normal(a, b, e), cc_atom(n - a, k - b, b == 0 ? e : false));
+		var ex = prod(cc_edge(a, b, e), cc_vert(n - a, k - b, b == 0 ? e : false));
 
 		rv = rv.concat( ex );
 
@@ -57,20 +100,20 @@ function c_atom(n, k, e) {
   return rv;
 }
 
-function c_normal(n, k, e) {
+function c_edge(n, k, e) {
   if (n < 0) return [];
   if (k == 0) e = false;
 
   if (n == 0 && k == 1 && e) {
 	 return [["var"]];
   }
-  var rv = cc_atom(n - 1, k + 1, e).map(function(x) { return ["lam", x] });
+  var rv = cc_vert(n - 1, k + 1, e).map(function(x) { return ["lam", x] });
   for (var a = 0; a <= n; a++) {
 	 for (var b = 0; b <= k; b++) {
 
 		if (a == 0 && b == 0) continue;
 		if (a == n && b == k) continue;
-		rv = rv.concat( prod(cc_atom(a, b, e), cc_atom(n - a, k - b, b == 0 ? e :  false)) );
+		rv = rv.concat( prod(cc_vert(a, b, e), cc_vert(n - a, k - b, b == 0 ? e :  false)) );
 
 	 }
   }
@@ -81,15 +124,15 @@ function c_struct(n, k) {
   if (n < 0) return [];
   if (k < 0) return [];
 
-    var nn = n-1;
-  var rv = cc_normal(nn, k+1 , true).map(function(x) { return ["lam", x] });
+  var nn = n-1;
+  var rv = cc_edge(nn, k+1 , true).map(function(x) { return ["lam", x] });
 
 
   for (var a = 0; a <= n; a++) {
 	 for (var b = 0; b <= k; b++) {
 		if (a == 0 && b == 0) continue;
 		if (a == n && b == k) continue;
-		rv = rv.concat( prod(cc_normal(a, b, false), cc_normal(n - a, k - b, false)) );
+		rv = rv.concat( prod(cc_edge(a, b, false), cc_edge(n - a, k - b, false)) );
 	 }
 
   }
@@ -115,12 +158,12 @@ function ids(x) {
 }
 
 
-//console.log(cc_normal(1,0,false).map(string));
- //console.log(cc_struct(3,0).length);
+//console.log(cc_edge(1,0,false).map(string));
+//console.log(cc_vert(3,1).map(string));
 
 
 for(var i = 0; i < 7; i++) {
-  console.log(cc_struct(i,0).length);
+  console.log(cc_norm(i,0).length);
 }
 
 // subj.forEach(function(x) {
